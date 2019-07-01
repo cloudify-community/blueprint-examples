e2cd686000a4a60230bc973cbc33282ee82e3969
earthmant <trammell@cloudify.co>
2019-07-01 16:46:25 +0300

    make all that non imperative

--- kubernetes/scripts/install_netaddr.sh
+++ kubernetes/scripts/install_netaddr.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+ctx logger info "TRYING TO INSTALL IPADDR WE WILL NOT FAIL IF THIS FAILS"
+ctx logger info "YOU CAN ALSO RUN sudo yum install -y python-netaddr && /opt/mgmtworker/env/bin/pip install netaddr on the manager."
+
 RESULT=$(python -c "import ipaddr")
 
 if [[ "${RESULT}" != 0 ]]; then
     if [[ -n "$(command -v yum)" ]]; then
-        sudo yum install -y python-netaddr
+        sudo yum install -y python-netaddr || true
     elif [[ -n "$(command -v apt-get)" ]]; then
-        sudo apt-get install -y python-netaddr
+        sudo apt-get install -y python-netaddr || true
     fi
-    /opt/mgmtworker/env/bin/pip install netaddr
+    sudo /opt/mgmtworker/env/bin/pip install netaddr || true
 fi
