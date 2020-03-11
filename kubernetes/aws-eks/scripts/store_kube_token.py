4426fa18f8aa273d859717b877894b27ec2602c0
Ahmad Musa <ahmad@cloudify.co>
2020-03-11 21:38:46 +0200

    AWS EKS Example blueprint

--- kubernetes/aws-eks/scripts/store_kube_token.py
+++ kubernetes/aws-eks/scripts/store_kube_token.py
@@ -1,6 +1,8 @@
+import base64
+
 from cloudify.state import ctx_parameters as inputs
 from cloudify.manager import get_rest_client
 
 
 client = get_rest_client()
-client.secrets.create('kubernetes_token',  inputs['kube_token'])
+client.secrets.create('kubernetes_token',  base64.b64decode(inputs['kube_token']))
