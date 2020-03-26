1a3caf1dea1b6ddb1288b15c8a08027195f34096
AdarShaked <adar@cloudify.co>
2020-03-26 16:14:38 +0200

    fixed install http server directory in install.sh

--- getting-started/scripts/http-server/install.sh
+++ getting-started/scripts/http-server/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
-cd /tmp
-nohup http-server /tmp -p 8080  > /dev/null 2>&1 &
+cd ~
+nohup http-server ~ -p 8080  > /dev/null 2>&1 &
 PID=$!
 ctx instance runtime_properties pid ${PID}
 ctx logger info "PID is ${PID}"
