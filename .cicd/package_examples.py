6a19151fc91fc80f7e40fb0cc8259fc75f053f00
earthmant <trammell@cloudify.co>
2020-04-05 16:49:26 +0300

    update to use central repo for packaging

--- .cicd/package_examples.py
+++ .cicd/package_examples.py
@@ -13,181 +13,64 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import logging
-from os import environ, path, walk, remove
 from re import sub
-import shutil
-import sys
-from tempfile import NamedTemporaryFile
-import zipfile
 
-from github import Github
+from os import path
 
-from __init__ import (CWD,
-                      SUPPORTED_EXAMPLES,
-                      blueprint_list,
+from ecosystem_cicd_tools.release import (
+    blueprint_release_with_latest, get_most_recent_release)
+
+from __init__ import (SUPPORTED_EXAMPLES,
                       get_cloudify_version)
 
-ASSET_TYPE = 'zip'
-RELEASE_MESSAGE = """Example blueprints for use with Cloudify version {0}.
-This is package number {1} to be released for this version of Cloudify.
-Always try to use the latest package for your version of Cloudify."""
+BLUEPRINTS_RELEASE_NAME = 'Cloudify 5.0.5 Blueprint Examples Bundle v{0}'
 GETTING_STARTED = ['getting-started/mc-jboss.yaml',
                    'getting-started/mc-nodejs.yaml']
 
-logging.basicConfig(level=logging.INFO)
+
+def get_last_version():
+    latest_release = get_most_recent_release()
+    return getattr(latest_release, 'tag_name', None)
 
 
-class NewRelease(object):
-    """Handles Creating Releases and uploading artifacts."""
-
-    def __init__(self):
-        self.client = Github(environ['RELEASE_BUILD_TOKEN'])
-        self.repo = \
-            self.client.get_repo(
-                '{org}/{repo}'.format(
-                    org=environ['CIRCLE_PROJECT_USERNAME'],
-                    repo=environ['CIRCLE_PROJECT_REPONAME']
-                )
-            )
-        self.commit = self.repo.get_commit(environ['CIRCLE_SHA1'])
-        self._version = None
-
-        if self.create_new_release():
-            self._release = self._create()
-        else:
-            self._release = None
-
-    def create_new_release(self, create_new_release=False):
-        if create_new_release:
-            return True
-        for commit_file in self.commit.files:
-            if commit_file.filename in \
-                    [path.relpath(f) for f in blueprint_list]:
-                return True
-        logging.info(
-            'No changes to blueprint files, not creating new release.')
-        return False
-
-    @property
-    def version(self):
-        if self._version:
-            return self._version
-        blueprints_version = get_cloudify_version()
-        version = self._get_last_version()
-        logging.info(
-            'Old version: {0}. New version: {1}'.format(
-                blueprints_version, version))
-        if blueprints_version > version:
-            version = blueprints_version
-        try:
-            self._version = '{0}-{1}'.format(
-                version.split('-')[0],
-                str(int(version.split('-')[1]) + 1))
-        except IndexError:
-            self._version = '{0}-{1}'.format(version, str(0))
-        return self._version
-
-    @property
-    def release(self):
-        return self._release
-
-    @property
-    def name(self):
-        gen, iteration = self.version.split('-')
-        return 'Cloudify {0} Blueprint Examples ' \
-               'Bundle v{1}'.format(gen, iteration)
-
-    @property
-    def message(self):
-        gen, iteration = self.version.split('-')
-        return RELEASE_MESSAGE.format(gen, iteration)
-
-    def _create(self):
-        logging.info('Attempting to create new release.')
-        return self.repo.create_git_release(
-            tag=self.version,
-            name=self.name,
-            message=self.message,
-            target_commitish=self.commit)
-
-    def upload(self, asset_path, basename):
-        logging.info('Attempting upload new archive {0}:{1}.'.format(
-            asset_path, basename))
-        asset_label = '{0}-{1}.{2}'.format(basename, self.version, ASSET_TYPE)
-        if not self.release:
-            return
-        self.release.upload_asset(asset_path, asset_label)
-
-    def _get_last_version(self):
-        try:
-            return str(self.repo.get_releases()[0].tag_name)
-        except (IndexError, KeyError):
-            return None
-
-    def update_getting_started(self, file_path):
-        logging.info('Updating Getting Started!')
-        if not path.exists(file_path):
-            raise Exception('Tried to update getting started but failed.')
-        lines = open(file_path, 'r').readlines()
-        lines = [sub(r"[\d\.]+\-[\d]+", self.version, l) for l in lines]
-        for line in lines:
-            logging.info('Line: {0}'.format(line))
-        f = open(file_path, 'w')
-        f.writelines(lines)
-        f.close()
+def get_blueprints_version():
+    blueprints_version = get_cloudify_version()
+    version = get_last_version()
+    logging.info(
+        'Old version: {0}. New version: {1}'.format(
+            blueprints_version, version))
+    version = blueprints_version if blueprints_version > version else version
+    try:
+        _version = '{0}-{1}'.format(version.split('-')[0], str(int(version.split('-')[1]) + 1))
+    except IndexError:
+        _version = '{0}-{1}'.format(version, str(0))
+    return _version
 
 
-class BlueprintArchive(object):
-    """Handles creating zip archives of blueprints."""
-
-    def __init__(self, name, source_directory):
-        if '/' in name:
-            name = name.replace('/', '-')
-            name = name.strip('-')
-        self.name = name
-        self.source = source_directory
-        self._dest = NamedTemporaryFile(delete=False)
-        self.destination = path.join(
-            path.dirname(self._dest.name), '{0}.zip'.format(self.name))
-        self._create_archive()
-        logging.info('Moving {0} to {1}.'.format(
-            self._dest.name, self.destination))
-        shutil.move(self._dest.name, self.destination)
-
-    def _create_archive(self):
-        logging.info('Archive source: {0}.'.format(self.source))
-        logging.info('Archive destination: {0}.'.format(self._dest.name))
-        zip_file = zipfile.ZipFile(self._dest.name, 'w')
-        for root, _, files in walk(self.source):
-            for filename in files:
-                file_path = path.join(root, filename)
-                source_dir = path.dirname(self.source)
-                zip_file.write(
-                    file_path, path.relpath(file_path, source_dir))
-        zip_file.close()
-        logging.info('Finished writing archive {0}'.format(self.name))
+def update_getting_started(file_path, version):
+    logging.info('Updating Getting Started!')
+    if not path.exists(file_path):
+        raise Exception('Tried to update getting started but failed.')
+    lines = open(file_path, 'r').readlines()
+    lines = [sub(r"[\d\.]+\-[\d]+", version, l) for l in lines]
+    for line in lines:
+        logging.info('Line: {0}'.format(line))
+    f = open(file_path, 'w')
+    f.writelines(lines)
+    f.close()
 
 
-if __name__ == "__main__":
-    """Create a new release in Github and
-    upload zip archives of all the blueprints.
-    """
+if __name__ == '__main__':
 
-    new_release = NewRelease()
-
-    if not new_release.release:
-        logging.info('No new release to upload new archives to.')
-        sys.exit()
-
+    blueprint_examples_version = get_blueprints_version()
+    blueprint_release_name = BLUEPRINTS_RELEASE_NAME.format(
+        blueprint_examples_version)
     for gs in GETTING_STARTED:
-        new_release.update_getting_started(gs)
-
-    for blueprint_id, file_path in SUPPORTED_EXAMPLES.items():
-        logging.info('Attempting to create new zip {0}.'.format(blueprint_id))
-        new_archive = BlueprintArchive(
-            blueprint_id,
-            path.join(CWD, blueprint_id)
-        )
-        new_release.upload(new_archive.destination, new_archive.name)
-        remove(new_archive.destination)
+        update_getting_started(gs, blueprint_examples_version)
+    blueprint_release_with_latest(
+        'blueprint-examples',
+        blueprint_examples_version,
+        blueprint_release_name,
+        SUPPORTED_EXAMPLES)
